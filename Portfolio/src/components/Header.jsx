1d31dabcf73f64363a61c8f3568716c8d460eee8
Hernan Sanchez <hernansanchez395@gmail.com>
2024-05-22 00:23:38 -0300

    Se agrego informacion personal del alumno en Main

--- Portfolio/src/components/Header.jsx
+++ Portfolio/src/components/Header.jsx
@@ -6,10 +6,10 @@ import Hernan from "../assets/Hernan.jpg";
 
 const Header = () => {
   return (
-    <div className="fondo">
+    <div className="fondoHeader py-3">
       <Container className=" container-fluid text-center">
         <Image src={Hernan} className="avatar" roundedCircle />
-        <h1 className="text-light mt-3">SÁNCHEZ ORTIZ HERNÁN</h1>
+        <h1 className=" mt-3">SÁNCHEZ ORTIZ HERNÁN</h1>
         <p className="lead">Desarrollador Fullstack</p>
       </Container>
     </div>
--- Portfolio/src/components/Main.jsx
+++ Portfolio/src/components/Main.jsx
@@ -1,12 +1,45 @@
-import React from 'react'
+import React from "react";
 import "../styles/styles.css";
+import { Container } from "react-bootstrap";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHtml5 } from '@fortawesome/free-brands-svg-icons'
 
 const Main = () => {
-  return (
-    <div>
-      <p>Main</p>
-    </div>
-  )
-}
+  const Alumno = {
+    nombre:"Hernán Sánchez Ortiz",
+    telefono:"+54 381 3331098",
+    email:"hernansanchez395@gmail.com",
+    ubicacion:"San Miguel de Tucumán",
+    edad:"28 años"
+  }
 
-export default Main
+  return (
+    <div className="py-1 fondoMain">
+      <Container className="my-4 shadow p-3 rounded bg-white">
+        <h3 className="text-secondary">Datos personales</h3>
+        <hr />
+        <p><strong>Email: </strong>{Alumno.email}</p>
+        <p><strong>Teléfono: </strong>{Alumno.telefono}</p>
+        <p><strong>Ubicación: </strong>{Alumno.ubicacion}</p>
+        <p><strong>Edad: </strong>{Alumno.edad}</p>
+        <hr />
+        <h3 className="text-secondary">Sobre mí:</h3>
+        <p>
+          Hola! Soy {Alumno.nombre}, Programador Fullstack, soy una persona
+          autodidacta, proactiva, con capacidad de trabajar en equipo y aprender
+          rápidamente. Me gusta la programación entre otras cosas por sus
+          desafíos y también me gusta buscar soluciones a todos los problemas
+          que puedan presentarse.
+        </p>
+      </Container>
+      <Container className="shadow mb-5 p-3 bg-white rounded">
+      <h3 className="text-secondary">Habilidades</h3>
+      <hr />
+      <strong><FontAwesomeIcon className="iconos" icon={faHtml5} /> HTML 5</strong>
+      
+      </Container>
+    </div>
+  );
+};
+
+export default Main;
